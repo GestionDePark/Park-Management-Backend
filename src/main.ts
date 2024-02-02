895e2689490da81f8ec97087b62c5807480f3527
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-02 15:23:46 +0300

    feat: swagger api generator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config: Omit<OpenAPIObject, "paths"> = new DocumentBuilder()
+        .setTitle('GestionDePark')
+        .setDescription('Manage your park easily')
+        .setVersion('3.0.3')
+        .build();
+    const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(8080);
 }
 bootstrap();
