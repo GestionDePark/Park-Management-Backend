fc0f468f132e66d54e3cb89206623420aa31a1b7
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-05 23:39:02 +0300

    feat: RDS connection

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -18,10 +18,10 @@ export default (): Config => ({
     port: parseInt(process.env.APP_PORT, 10),
     databaseConfig: {
         type: 'postgres',
-        host: process.env.DATABASE_HOST || 'localhost',
-        port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
+        host: process.env.RDS_HOSTNAME || 'localhost',
+        port: parseInt(process.env.RDS_PORT, 10) || 5432,
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
     },
 });
