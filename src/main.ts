71a7faa6b8f64061b51580f9d588ecf6277a7b53
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-06 14:14:56 +0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ParseIntPipe, ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({origin: "*"});
     app.useGlobalPipes(new ValidationPipe());
 
     const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
