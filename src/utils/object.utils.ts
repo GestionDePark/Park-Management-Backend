eb319fe39dbd07650e6415343d6f8d6e61c6c352
kirisaki_vk <kirisaki.vk@gmail.com>
2024-02-13 18:31:31 +0300

    fix: non-valid auth error

--- src/utils/object.utils.ts
+++ src/utils/object.utils.ts
@@ -2,15 +2,17 @@ export function omit<T extends object, K extends keyof T>(
     obj: T,
     keys: K[],
 ): Omit<T, K> {
-    return Object.keys(obj).reduce(
-        (o, key) => {
-            if (!keys.includes(key as K)) {
-                o[key] = obj[key];
-            }
-            return o;
-        },
-        {} as Omit<T, K>,
-    );
+    return obj
+        ? Object.keys(obj).reduce(
+              (o, key) => {
+                  if (!keys.includes(key as K)) {
+                      o[key] = obj[key];
+                  }
+                  return o;
+              },
+              {} as Omit<T, K>,
+          )
+        : null;
 }
 
 export type AuthenticatedRequest = Request & {
